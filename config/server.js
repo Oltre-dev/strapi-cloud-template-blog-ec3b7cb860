af13a12603ce7dc3fe610a8c40a8ce6f9ac293f7
Julien <jjy.quere@gmail.com>
2025-06-10 17:59:35 +0200

    Add github webhook management

--- config/server.js
+++ config/server.js
@@ -1,10 +1,15 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
   webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    defaultHeaders: {
+      Authorization: `Bearer ${env("GITHUB_WEBHOOK_TOKEN")}`,
+      "Content-Type": "application/json",
+      "User-Agent": "Strapi-Blog-Webhook/1.0",
+    },
   },
 });
