a16d9ffc90fd63b35c603b07ae1e298113cb8566
Julien <jjy.quere@gmail.com>
2025-06-10 18:26:03 +0200

    Revert "New webhook management"
    This reverts commit 173f242cceb73c461892f5e294278b912c3ea2f5.

--- src/index.js
+++ src/index.js
@@ -1,48 +1,21 @@
-"use strict";
+'use strict';
 const bootstrap = require("./bootstrap");
 
-const triggerDispatch = require("./utils/github-dispatch");
-
 module.exports = {
-  async bootstrap({ strapi }) {
-    strapi.db.lifecycles.subscribe({
-      // `models: []` ⇒ toutes les content-types
-      async beforeUpdate(event) {
-        // On mémorise l’état précédent pour savoir si on passe de non-publié à publié
-        const { where } = event.params;
-        if (where?.id) {
-          const uid = event.model.uid;
-          event.state = {
-            prev: await strapi.entityService.findOne(uid, where.id, {
-              fields: ["publishedAt"],
-            }),
-          };
-        }
-      },
+  /**
+   * An asynchronous register function that runs before
+   * your application is initialized.
+   *
+   * This gives you an opportunity to extend code.
+   */
+  register(/*{ strapi }*/) {},
 
-      async afterUpdate(event) {
-        const { result } = event;
-        const wasDraft = !event.state?.prev?.publishedAt;
-        const isNowLive = !!result.publishedAt;
-        if (wasDraft && isNowLive) {
-          await triggerDispatch("strapi-publish", {
-            model: event.model.uid,
-            id: result.id,
-            slug: result.slug || result.title || result.id,
-          });
-        }
-      },
-
-      async afterCreate(event) {
-        // Cas où l’entrée est créée directement en 'published'
-        const { result } = event;
-        if (result.publishedAt) {
-          await triggerDispatch("strapi-publish", {
-            model: event.model.uid,
-            id: result.id,
-          });
-        }
-      },
-    });
-  },
+  /**
+   * An asynchronous bootstrap function that runs before
+   * your application gets started.
+   *
+   * This gives you an opportunity to set up your data model,
+   * run jobs, or perform some special logic.
+   */
+  bootstrap,
 };
